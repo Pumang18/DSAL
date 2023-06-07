070a066602afe6160dc512002e619ba0b4255f1c
Pumang Kirange <112180951+Pumang18@users.noreply.github.com>
2023-06-07 18:52:55 +0530

    Update A11Adt.cpp

--- A11Adt.cpp
+++ A11Adt.cpp
@@ -1,15 +1,3 @@
-/*
-Name : Gaurav Gorane SCOA10
-PROBLEM STATEMENT: To create ADT that implements the SET concept.
-    a. Add (newElement) -Place a value into the set
-    b. Remove (element) Remove the value
-    c. Contains (element) Return true if element is in collection
-    d. Size () Return number of values in collection Iterator () Return an iterator used to loop over collection
-    e. Intersection  of two sets
-    f. Union of two sets
-    g. Difference between two sets
-    h.Subset
-*/
 
 #include <iostream>
 #include <list>
