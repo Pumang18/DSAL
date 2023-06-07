c1b8b64b4eee03d73fb5c4010464847626e4258a
Pumang Kirange <112180951+Pumang18@users.noreply.github.com>
2023-06-07 18:55:15 +0530

    Update A8obst.cpp

--- A8obst.cpp
+++ A8obst.cpp
@@ -1,5 +1,4 @@
 /*
-Name : Gaurav Gorane SC0A10
 Problem Statement : Given sequence k = k1 <k2 < ... < kn of n sorted keys, with a search 
 probability pi  for  each  key  ki. Build  the  Binary  search tree  that  has  the  least 
  search  cost  given  the  access probability for each key. 
