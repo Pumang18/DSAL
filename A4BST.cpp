1ccd22f5ed3f0eb24cf2ba4ad462a57c53f85b66
Pumang Kirange <112180951+Pumang18@users.noreply.github.com>
2023-06-07 18:54:28 +0530

    Update A4BST.cpp

--- A4BST.cpp
+++ A4BST.cpp
@@ -1,15 +1,3 @@
-/*
-Name : Gaurav Gorane SC0A10
-
-Problem Statement : Beginning with an empty binary search tree, Construct binary search tree by inserting the values in the order given. After constructing a binary tree -
-i. Insert new node
-ii. Find number of nodes in longest path
-iii. Minimum data value found in the tree
-iv. Change a tree so that the roles of the left and right pointers are swapped at every node
-v. Search a value
-
-*/
-
 #include<iostream>
 using namespace std;
 class Node
